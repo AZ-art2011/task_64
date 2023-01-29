98a8c84034f519d68ad009475e1064b33ab129f1
Alexander <mypost@example.com>
2023-01-29 11:14:59 +0300

    2

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 Console.WriteLine("Укажите верхнюю границу > 1:");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
 void Count (int num)
 {
     if (num < 0) Console.Write($"{num} не натуральное число");
     if (num == 0) return;
-    Console.Write("{0,4}", num);
+    Console.Write($"{num} ");
     Count (num - 1);
 }
 
